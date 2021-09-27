9403a3d36ad0f3f8f7f551d600ce496b9d6f2309
Fred Gryson <fgryson@gmail.com>
2021-09-27 18:20:04 +0100

    Added: Random Inspirational quotes API

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,10 @@ document.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     document.documentElement.scrollLeft += evt.deltaY;
 });
+
+fetch("https://type.fit/api/quotes")
+  .then((response) => response.json())
+  .then((data) => {
+    let randomQuote = data[Math.floor(Math.random()*data.length)];
+    $("#quote").children("p").text(`"${randomQuote.text}"\n- ${randomQuote.author}`)
+  });
